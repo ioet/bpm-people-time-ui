ff4fe8a95290d3d1909e945c94761d107c482623
oemel09 <github@oemel09.de>
2018-12-14 12:19:42 -0500

    BPM-56 Login works now

--- src/component/login-page/LoginPageContainer.jsx
+++ src/component/login-page/LoginPageContainer.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import LoginPage from './LoginPage';
-import isUserLoggedIn from './login-selector';
+import { isUserLoggedIn } from './login-selector';
 
 const mapStateToProps = state => ({
   isLoggedIn: isUserLoggedIn(state),

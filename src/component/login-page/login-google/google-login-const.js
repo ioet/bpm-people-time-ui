a35884cc72d81e2f501652642fdc65560f4cbabf
oemel09 <github@oemel09.de>
2018-12-14 14:31:41 -0500

    BPM-56 Configured new google project

--- src/component/login-page/login-google/google-login-const.js
+++ src/component/login-page/login-google/google-login-const.js
@@ -1,5 +1,5 @@
 const GoogleLoginConst = {
-  GOOGLE_CLIENT_ID: '1070728078213-1b275jha3vmluup1n9asvjdbsjmkenc0.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '539589108546-du8sgge5jsrtn9u85g3olhtsbkh7mp1b.apps.googleusercontent.com',
   IOET_DOMAIN: 'ioet.com',
 };
 
